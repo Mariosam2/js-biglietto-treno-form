5bc833e76921297798ed722312c80ef4f5ac2863
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-07 23:27:06 +0200

    adding exercise without form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,52 @@
-const form = document.getElementById("ticketForm");
-const inputs = document.getElementById("ticketForm").elements;
-const cancel = document.getElementById("cancel");
-const userName = document.getElementById("name");
-const surname = document.getElementById("surname");
-const ageGroup = document.getElementById("ageGroup");
-const price = document.getElementById("price");
+const inputs = document.querySelectorAll('input, select');
+const button = document.getElementById('generate');
+const cancel = document.getElementById('cancel');
+const userAge = document.getElementById('userAge');
+const userName = document.getElementById('name');
+const ageGroup = document.getElementById('ageGroup');
+const price = document.getElementById('price');
 let discount = 20;
-reset();
-// calculation for the price
-function getPrice(value) {
-    let price = value * 0.21;
-    let discountedPrice = (price/100)* discount;
-    price = price - discountedPrice;
-    if(!Number(price)){
-        price = 0;
-    }
-    price = price.toFixed(2);
-    return price.toString();
+
+function reset (){
+    inputs.forEach((input)=> {
+        if (input.type == "text") {
+            input.value = "";
+        }
+    })
+    userName.innerHTML = "";
+    price.innerHTML = "";
+    userAge.innerHTML = "";
 }
+
+function setDiscount(value){
+    if (value == "underAge") {
+        discount = 20;
+        
+        
+    } else if (value == "overAge") {
+        discount = 40;
+        
+        
+    }  else {
+        discount = 0;
+        
+
+    }
+}
+
 function getAgeGroup (value){
     let innerText;
     if (value == "underAge") {
-        discount = 20;
         innerText = "Minorenne"
         return innerText;
         
         
     } else if (value == "overAge") {
-        discount = 40
         innerText = "Over 65"
         return innerText;
         
         
     }  else {
-        discount = 0;
         innerText = "Nessuno Sconto"
         return innerText;
         
@@ -41,78 +54,35 @@ function getAgeGroup (value){
     }
 }
 
-
-// resetting the state of the page as default
-function reset () {
-    for (let [key, value] of Object.entries(inputs)){
-        if(value.type == "text"){
-            value.value = "";
-        } if (value.type == "select-one"){
-            value.value = value[0].value;
-        }
-        
-    }
-    userName.innerHTML = "";
-    surname.innerHTML = "";
-    ageGroup.innerHTML = "Minorenne"
-    price.innerHTML = "0.00&euro;";
+function getPrice(value) {
+    let price = value * 0.21;
+    let discountedPrice = (price/100)* discount;
+    price = price - discountedPrice;
+    price = price.toFixed(2);
+    return price.toString();
 }
 
-// Saving input values
-inputs["userName"].addEventListener("input", ()=>{
-    if(!inputs["userName"].value.match(/^[A-Z]+$/gi)){
-        inputs["userName"].classList.add("error")
-    } else {
-        inputs["userName"].classList.remove("error")
-    }
-    userName.innerHTML = inputs["userName"].value;
+
+
+
+button.addEventListener('click',()=>{
+    setDiscount(userAge.value);
+    inputs.forEach((input)=>{
+        if(input.id == "userAge"){
+            console.log(input.value);
+            ageGroup.innerHTML = getAgeGroup(input.value);
+        }
+        if(input.id == "userName"){
+        userName.innerHTML = input.value;
+        }
+        if(input.id == "distance"){
+            price.innerHTML = getPrice(input.value);
+        }
+    })
+
 })
 
-
-inputs["userSurname"].addEventListener("input", ()=>{
-    if(!inputs["userSurname"].value.match(/^[A-Z]+$/gi)){
-        inputs["userSurname"].classList.add("error")
-    } else {
-        inputs["userSurname"].classList.remove("error")
-    }
-    surname.innerHTML = inputs["userSurname"].value;
-})
-
-inputs["distance"].addEventListener("input", ()=> {
-    if(!Number(inputs["distance"].value)){
-        inputs["distance"].classList.add("error");
-        
-    } else {
-        inputs["distance"].classList.remove("error");
-    }
-    
-    price.innerHTML = getPrice(inputs["distance"].value) + "&euro;"  
-})
-
-inputs["userAge"].addEventListener("input", ()=>{
-    ageGroup.innerHTML =  getAgeGroup(inputs["userAge"].value);
-    price.innerHTML = getPrice(inputs["distance"].value) + "&euro;"
-})
-
-cancel.addEventListener("click", ()=>{
+cancel.addEventListener('click', ()=>{
     reset();
-    
 })
-
-// Form validation
-form.addEventListener("submit", (event) => {
-    if(!Number(inputs["distance"].value)) {
-        event.preventDefault();
-        
-    }
-    if (!(inputs["userName"].value.match(/^[A-Z]+$/gi) && inputs["userSurname"].value.match(/^[A-Z]+$/gi))){
-        event.preventDefault()
-    }
-    localStorage.setItem("userName", inputs["userName"].value);
-    localStorage.setItem("userSurname", inputs["userSurname"].value);
-    localStorage.setItem("ageGroup", getAgeGroup(inputs["userAge"].value));
-    localStorage.setItem("price", getPrice(inputs["distance"].value));
-    
-});
-
 
---
+++ bonus/assets/js/main.js
@@ -0,0 +1,118 @@
+const form = document.getElementById("ticketForm");
+const inputs = document.getElementById("ticketForm").elements;
+const cancel = document.getElementById("cancel");
+const userName = document.getElementById("name");
+const surname = document.getElementById("surname");
+const ageGroup = document.getElementById("ageGroup");
+const price = document.getElementById("price");
+let discount = 20;
+reset();
+// calculation for the price
+function getPrice(value) {
+    let price = value * 0.21;
+    let discountedPrice = (price/100)* discount;
+    price = price - discountedPrice;
+    if(!Number(price)){
+        price = 0;
+    }
+    price = price.toFixed(2);
+    return price.toString();
+}
+function getAgeGroup (value){
+    let innerText;
+    if (value == "underAge") {
+        discount = 20;
+        innerText = "Minorenne"
+        return innerText;
+        
+        
+    } else if (value == "overAge") {
+        discount = 40
+        innerText = "Over 65"
+        return innerText;
+        
+        
+    }  else {
+        discount = 0;
+        innerText = "Nessuno Sconto"
+        return innerText;
+        
+
+    }
+}
+
+
+// resetting the state of the page as default
+function reset () {
+    for (let [key, value] of Object.entries(inputs)){
+        if(value.type == "text"){
+            value.value = "";
+        } if (value.type == "select-one"){
+            value.value = value[0].value;
+        }
+        
+    }
+    userName.innerHTML = "";
+    surname.innerHTML = "";
+    ageGroup.innerHTML = "Minorenne"
+    price.innerHTML = "0.00&euro;";
+}
+
+// Saving input values
+inputs["userName"].addEventListener("input", ()=>{
+    if(!inputs["userName"].value.match(/^[A-Z]+$/gi)){
+        inputs["userName"].classList.add("error")
+    } else {
+        inputs["userName"].classList.remove("error")
+    }
+    userName.innerHTML = inputs["userName"].value;
+})
+
+
+inputs["userSurname"].addEventListener("input", ()=>{
+    if(!inputs["userSurname"].value.match(/^[A-Z]+$/gi)){
+        inputs["userSurname"].classList.add("error")
+    } else {
+        inputs["userSurname"].classList.remove("error")
+    }
+    surname.innerHTML = inputs["userSurname"].value;
+})
+
+inputs["distance"].addEventListener("input", ()=> {
+    if(!Number(inputs["distance"].value)){
+        inputs["distance"].classList.add("error");
+        
+    } else {
+        inputs["distance"].classList.remove("error");
+    }
+    
+    price.innerHTML = getPrice(inputs["distance"].value) + "&euro;"  
+})
+
+inputs["userAge"].addEventListener("input", ()=>{
+    ageGroup.innerHTML =  getAgeGroup(inputs["userAge"].value);
+    price.innerHTML = getPrice(inputs["distance"].value) + "&euro;"
+})
+
+cancel.addEventListener("click", ()=>{
+    reset();
+    
+})
+
+// Form validation
+form.addEventListener("submit", (event) => {
+    if(!Number(inputs["distance"].value)) {
+        event.preventDefault();
+        
+    }
+    if (!(inputs["userName"].value.match(/^[A-Z]+$/gi) && inputs["userSurname"].value.match(/^[A-Z]+$/gi))){
+        event.preventDefault()
+    }
+    localStorage.setItem("userName", inputs["userName"].value);
+    localStorage.setItem("userSurname", inputs["userSurname"].value);
+    localStorage.setItem("ageGroup", getAgeGroup(inputs["userAge"].value));
+    localStorage.setItem("price", getPrice(inputs["distance"].value));
+    
+});
+
+
